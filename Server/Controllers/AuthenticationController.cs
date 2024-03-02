bf6a9c43af70fbb888f1715bb002e71638c72c93
rovinsharmagit <internetsharma45@gmail.com>
2024-03-02 19:49:26 +0530

    Adding Register User In Authentication Controller

---
+++ Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using BaseLibrary.DTOs; 
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServerLibrary.Repositories.Contracts;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IUserAccount _userAccount; // Use dependency injection
+
+        public AuthenticationController(IUserAccount userAccount)
+        {
+            _userAccount = userAccount; // Assign injected dependency
+        }
+
+        //Method For Register the New User
+        [HttpPost("register")]
+        public async Task<IActionResult> CreateAsync(Register user)
+        {
+            if (user == null)
+            {
+                return BadRequest(new { message = "Model is empty" }); 
+            }
+
+            var result = await _userAccount.CreateAsync(user);
+            return Ok(result);
+        }
+    }
+}
